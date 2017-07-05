00404b8778218606d140cfd227e7a7579b7add0d
Tony Bogdanov <tonybogdanov@gmail.com>
2017-07-05 13:24:35 +0300

    init

---
+++ classes/Bundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  @package    TB Asset Management
+ *  @author     Tony Bogdanov <support@tonybogdanov.com>
+ *  @license    MIT http://www.opensource.org/licenses/mit-license.php
+ *  @copyright  Copyright (c) 2017. www.tonybogdanov.com. All Rights Reserved.
+ */
+
+namespace TB\AssetManagement;
+
+class Bundle implements \TB\Bundle
+{
+
+}
